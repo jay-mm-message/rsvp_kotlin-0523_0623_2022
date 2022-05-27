4bdf2cf9d36783ff19b0dde5de604d0cbb350a70
jcwang <jcwang@sonicwall.com>
2022-05-28 00:42:49 +0800

    Add unit test.

--- DiceRollerImage/app/src/test/java/com/example/dicerollerimage/ExampleUnitTest.kt
+++ DiceRollerImage/app/src/test/java/com/example/dicerollerimage/ExampleUnitTest.kt
@@ -10,8 +10,16 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+//    @Test
+//    fun addition_isCorrect() {
+//        assertEquals(4, 2 + 2)
+//    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        val msg = "The value of rollResult was not between 1 and 6"
+        assertTrue(msg, rollResult in 1..6);
     }
 }
