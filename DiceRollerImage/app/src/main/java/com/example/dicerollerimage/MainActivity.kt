f1eede2c8cd1bc6cd8176c67ddac9712cf172f02
jcwang <jcwang@sonicwall.com>
2022-05-27 23:46:14 +0800

    Change condition from used by if to used by when.

--- DiceRollerImage/app/src/main/java/com/example/dicerollerimage/MainActivity.kt
+++ DiceRollerImage/app/src/main/java/com/example/dicerollerimage/MainActivity.kt
@@ -19,20 +19,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun diceRoll() {
         val diceImage: ImageView = findViewById(R.id.imageView)
-        val sideNum = Dice(6).roll()
 
-        if (sideNum == 1) {
-            diceImage.setImageResource(R.drawable.dice_1)
-        } else if (sideNum == 2) {
-            diceImage.setImageResource(R.drawable.dice_2)
-        } else if (sideNum == 3) {
-            diceImage.setImageResource(R.drawable.dice_3)
-        } else if (sideNum == 4) {
-            diceImage.setImageResource(R.drawable.dice_4)
-        } else if (sideNum == 5) {
-            diceImage.setImageResource(R.drawable.dice_5)
-        } else if (sideNum == 6) {
-            diceImage.setImageResource(R.drawable.dice_6)
+        when (Dice(6).roll()) {
+            1 -> {
+                diceImage.setImageResource(R.drawable.dice_1)
+            }
+            2 -> {
+                diceImage.setImageResource(R.drawable.dice_2)
+            }
+            3 -> {
+                diceImage.setImageResource(R.drawable.dice_3)
+            }
+            4 -> {
+                diceImage.setImageResource(R.drawable.dice_4)
+            }
+            5 -> {
+                diceImage.setImageResource(R.drawable.dice_5)
+            }
+            6 -> {
+                diceImage.setImageResource(R.drawable.dice_6)
+            }
         }
     }
 }
