a7889fec4a8b16c17c5b3c579d68e6ade3390624
jcwang <jcwang@sonicwall.com>
2022-05-28 00:42:24 +0800

    Add debug log.

--- DiceRollerImage/app/src/main/java/com/example/dicerollerimage/MainActivity.kt
+++ DiceRollerImage/app/src/main/java/com/example/dicerollerimage/MainActivity.kt
@@ -2,20 +2,37 @@ package com.example.dicerollerimage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private final val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var rollButton: Button = findViewById(R.id.rollButton)
+        val rollButton: Button = findViewById(R.id.rollButton)
 
         rollButton.setOnClickListener {
             diceRoll()
         }
+
+        logging()
     }
+
+    private fun logging() {
+        Log.e(TAG, "----------")
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+        Log.v(TAG, "----------")
+    }
+
 
     private fun diceRoll() {
 
