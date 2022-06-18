0ac111631ec12a9e0438a2ad616fd0e869a0bf8a
jcwang <jcwang@sonicwall.com>
2022-06-18 23:06:38 +0800

    Learn kotlin basic "E: [Lean: 瀏覽不同畫面, Android Kotlin 基本概念的相關詞彙, Kotlin 集合, List 類別, Set 類別, Map 類別, Collection 變化, 高階函式和 Lambdas, 函式類型, it: 單一參數的隱含名稱, Lambda 函式, 高階函式]".

--- DrawBoardSpace/app/src/main/java/com/example/drawboardspace/MainActivity.kt
+++ DrawBoardSpace/app/src/main/java/com/example/drawboardspace/MainActivity.kt
@@ -5,16 +5,78 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-
-    private val TAG: String = "Draw_Board";
+    val TAG: String = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val numbers = listOf<Int>(1, 2, 3, 5, 5)
-        Log.e(TAG, "${numbers}")
+        /**
+         *
+        Android Kotlin 基本概念的相關詞彙
+        Kotlin 集合
+        List 類別
+        Set 類別
+        Map 類別
+        Collection 變化
+        高階函式和 Lambdas
+        函式類型
+        it: 單一參數的隱含名稱
+        Lambda 函式
+        高階函式
+         */
 
-        val setOfNumbers = numbers.toSet()
-        Log.e(TAG, "${setOfNumbers}")
+        val topic1= listOf(
+            "Lean: 瀏覽不同畫面",
+            "Android Kotlin 基本概念的相關詞彙",
+                    "Kotlin 集合",
+                    "List 類別",
+                    "Set 類別",
+                    "Map 類別",
+                    "Collection 變化",
+                    "高階函式和 Lambdas",
+                    "函式類型",
+                    "it: 單一參數的隱含名稱",
+                    "Lambda 函式",
+                    "高階函式")
+        Log.e(TAG, "${topic1}")
+
+        val words = listOf("about", "acute", "awesome", "balloon", "best", "brief", "class", "coffee", "creative")
+        Log.e(TAG, words.filter { it.startsWith("a", ignoreCase = true) }.toString())
+
+        val num: Int = 10
+        val triple: (Int)->Int = { it * 3 }
+        Log.e(TAG, "triple(${num}): ${triple(num)}")
+
+        val peopleAges = mutableMapOf<String, Int>(
+            "Jay" to 100,
+            "Ellen" to 50
+        )
+
+        peopleAges["Jay"] = 200
+
+        Log.e(TAG, "${peopleAges}")
+
+        peopleAges.forEach { Log.e(TAG, "${it.key} to ${it.value}")}
+        Log.e(TAG, peopleAges.map { "${it.key} to ${it.value}" }.toString())
+        System.out.println(peopleAges.map { "${it.key} is ${it.value}" }.joinToString(", ") )
+
+        var set1 = setOf<Int>(1, 2, 3)
+        var set2 = setOf<Int>(3, 2, 1)
+
+        if (set1 == set2) {
+            Log.e(TAG, "set1 equal set2")
+        } else {
+            Log.e(TAG, "set1 not equal set2")
+        }
+
+        var buf: List<Int> =
+            listOf<Int>(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
+
+        Log.e(TAG, "${buf}")
+
+        var serOfNumber =
+                buf.toSet()
+
+        Log.e(TAG, "${serOfNumber}")
     }
 }
